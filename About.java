52ebc8a223fd9ff7655710ca1ed75eebbf01b41b
Ashnikasharma <145584301+Ashnikasharma@users.noreply.github.com>
2024-02-07 17:58:09 +0530

    Update About.java

--- About.java
+++ About.java
@@ -25,7 +25,7 @@ public class About extends JFrame implements ActionListener{
         add(l2);
 
 
-        JLabel l3 = new JLabel("<html>Code for Interview<br>Youtube Channel Version 2021<br>2021 Code for Interview. All rights reserved<br><br>Notepad is a word processing program, <br>which allows changing of text in a computer file.<br>Notepad is a simple text editor for basic text-editing program<br> which enables computer users to create documents. </html>");
+        JLabel l3 = new JLabel("<html>Notepad<br>2023<br>All rights reserved<br><br>Notepad is a word processing program, <br>which allows changing of text in a computer file.<br>Notepad is a simple text editor for basic text-editing program<br> which enables computer users to create documents. </html>");
         l3.setFont(new Font("SAN_SERIF", Font.PLAIN, 18));
         l3.setBounds(150, 130, 500, 300);
         add(l3);
